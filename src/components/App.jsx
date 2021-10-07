70f4ce5b77fdde74c41d4951d33e288e78904acd
Rinadd <nagisa.rn1@gmail.com>
2021-10-07 16:27:33 +0900

    file

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Article from "./components/Article";
+
+function App() {
+    return (
+        <div>
+            <Article
+                title={'Reactレクチャー'}
+                content={'propsについて'}
+                />
+        </div>
+    );
+}
+
+export default App;
